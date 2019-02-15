1d32cc3a438f0448d1ab81fd6a834ab1b955634b
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-02-15 21:53:56 +0900

    deleted internal add app module

--- src/main.js
+++ src/main.js
@@ -5,14 +5,24 @@ import YoroiLedgerBridge from './yoroi-ledger-bridge';
 const init = async () => {
   try {
     const bridge = new YoroiLedgerBridge();
-    (bridge) ? onSuccess() : onError();
+    if (bridge) {
+      onSuccess(bridge);
+    } else {
+      onError()
+    }
   } catch(error) {
     onError(error);
   }
 }
 
-const onSuccess = () => {
+const onSuccess = async (bridge) => {
   console.log('Yoroi Extension Ledger hardware wallet bridge initialized...');
+  try {
+    const deviceVersion = await bridge.getConnectedDeviceVersion();
+    console.info(`Connected Ledger device version: ${JSON.stringify(deviceVersion)}`);
+  } catch (error) {
+    console.info('No Ledger Nano S device is connected to system USB port');
+  }
 }
 
 const onError = (error) => {
