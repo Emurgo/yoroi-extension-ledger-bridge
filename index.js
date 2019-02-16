7f409cac1d75ebfaf342b035def352d8a32b9f4d
sebastiengllmt <sebastiengllmt@gmail.com>
2019-02-16 13:52:27 +0900

    first step to fixup file

--- index.js
+++ index.js
@@ -1,79 +1,132 @@
-const {EventEmitter} = require('events')
-const HDKey = require('hdkey')
+// @flow
+
+const {EventEmitter} = require('events');
+const HDKey = require('hdkey');
 
 // see SLIP-0044
 const coinType = 1815; // Cardano
 
-const hdPathString = `m/44'/${coinType}'/0'`
-const BRIDGE_URL = 'https://emurgo.github.io/yoroi-extension-ledger-bridge/'
+const hdPathString = `m/44'/${coinType}'/0'`;
+const BRIDGE_URL = 'https://emurgo.github.io/yoroi-extension-ledger-bridge/';
+
+type Account = {
+  address: string,
+  balance: ?number,
+  index: number
+}
+
+type Options = {
+  hdPath: string,
+  bridgeUrl: string,
+  accounts: Array<Account>
+};
+
+type MessageType = {
+  target?: string,
+  action: string,
+  params: any
+};
+
+type CallbackType = any => boolean;
+
+export type BIP32Path = Array<number>;
+
+export type InputTypeUTxO = {|
+  txDataHex: string,
+  outputIndex: number,
+  path: BIP32Path
+|};
+ 
+export type OutputTypeAddress = {|
+  amountStr: string,
+  address58: string
+|};
+ 
+export type OutputTypeChange = {|
+  amountStr: string,
+  path: BIP32Path
+|};  
 
 class LedgerBridge extends EventEmitter {
-  constructor (opts = {}) {
-    super()
-    this.bridgeUrl = null
-    // pages of accounts on the device
-    this.page = 0
-    this.perPage = 5
 
-    this.hdk = new HDKey()
-    this.iframe = null
-    this.deserialize(opts)
-    this._setupIframe()
+  currPage: number;
+  perPage: number;
+  bridgeUrl: string;
+  hdk: HDKey;
+  iframe: HTMLIFrameElement;
+  hdPath: string;
+  bridgeUrl: string;
+  accounts: Array<Account>;
+  unlockedAccount: number
+  
+  constructor (opts: Options = {}) {
+    super();
+    this.bridgeUrl = BRIDGE_URL;
+    // pages of accounts on the device
+    this.currPage = 1;
+    this.perPage = 5;
+
+    this.hdk = new HDKey();
+
+    this.hdPath = opts.hdPath || hdPathString
+    this.bridgeUrl = opts.bridgeUrl || BRIDGE_URL;
+    this.accounts = opts.accounts || [];
+
+    this.unlockedAccount = 0;
+
+    this.iframe = _setupIframe(this.bridgeUrl);
   }
 
-  serialize () {
+  serialize (): Promise<Options> {
     return Promise.resolve({
       hdPath: this.hdPath,
-      accounts: this.accounts,
       bridgeUrl: this.bridgeUrl,
+      accounts: this.accounts,
     })
-  }
-
-  deserialize (opts = {}) {
-    this.hdPath = opts.hdPath || hdPathString
-    this.bridgeUrl = opts.bridgeUrl || BRIDGE_URL
-    this.accounts = opts.accounts || []
-    return Promise.resolve()
   }
 
   // =====================
   //   Account Selection
   // =====================
 
-  isUnlocked () {
-    return !!(this.hdk && this.hdk.publicKey)
+  isUnlocked (): boolean {
+    return !!(this.hdk && this.hdk.publicKey);
   }
 
-  setAccountToUnlock (index) {
-    this.unlockedAccount = parseInt(index, 10)
+  setAccountToUnlock (index: number): void {
+    this.unlockedAccount = parseInt(index, 10);
   }
 
-  setHdPath (hdPath) {
+  setHdPath (hdPath: string): void {
     // Reset HDKey if the path changes
     if (this.hdPath !== hdPath) {
-      this.hdk = new HDKey()
+      this.hdk = new HDKey();
     }
-    this.hdPath = hdPath
+    this.hdPath = hdPath;
   }
 
-  getFirstPage () {
-    this.page = 0
-    return this.__getPage(1)
+  getFirstPage (): Promise<Array<Account>>  {
+    this.currPage = 1;
+    return _getPage(this.currPage, this.perPage);
   }
 
-  getNextPage () {
-    return this.__getPage(1)
+  getNextPage (): Promise<Array<Account>>  {
+    this.currPage += 1;
+    return _getPage(this.currPage, this.perPage);
   }
 
-  getPreviousPage () {
-    return this.__getPage(-1)
+  getPreviousPage (): Promise<Array<Account>>  {
+    if (this.currPage > 1) {
+      this.currPage -= 1;
+    }
+    return _getPage(this.currPage, this.perPage);
   }
 
-  forgetDevice () {
-    this.accounts = []
-    this.page = 0
-    this.unlockedAccount = 0
-    this.hdk = new HDKey()
+  forgetDevice (): void {
+    this.accounts = [];
+    this.currPage = 0;
+    this.unlockedAccount = 0;
+    this.hdk = new HDKey();
   }
 
   // ==============================
@@ -81,27 +134,29 @@ class LedgerBridge extends EventEmitter {
   // ==============================
 
   /** Pass major+mintor+patch version to callback */
-  getVersion() {
-    this._sendMessage({
-      action: 'ledger-get-version',
-      params: {
+  getVersion(callback: CallbackType): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this._sendMessage({
+        action: 'ledger-get-version',
+        params: {
+        },
       },
-    },
-    ({success, payload}) => {
-      if (success) {
-        if (callback(payload)) {
-          resolve();
-        } else {
-          reject(new Error('Ledger: getVersion callback failed'))
+      ({success, payload}) => {
+        if (success) {
+          if (callback(payload)) {
+            resolve();
+          } else {
+            reject(new Error('Ledger: getVersion callback failed'))
+          }
         }
-      }
-    })
+      });
+    });
   }
 
   /** Get extended public key and pass publicKeyHex+chainCodeHex to callback */
-  getExtendedPublicKey() {
+  getExtendedPublicKey(callback: CallbackType): Promise<void> {
     return new Promise((resolve, reject) => {
-      let hdPath = _getHdPath();
+      let hdPath = _getHdPath(this.unlockedAccount);
 
       this._sendMessage({
         action: 'ledger-get-extended-public-key',
@@ -118,13 +173,13 @@ class LedgerBridge extends EventEmitter {
           }
         }
       })
-    })
+    });
   }
 
   /** Get derive address and pas address58 to callback */
-  deriveAddress() {
+  deriveAddress(callback: CallbackType): Promise<void> {
     return new Promise((resolve, reject) => {
-      let hdPath = _getHdPath();
+      let hdPath = _getHdPath(this.unlockedAccount);
 
       this._sendMessage({
         action: 'ledger-derive-address',
@@ -141,13 +196,17 @@ class LedgerBridge extends EventEmitter {
           }
         }
       })
-    })
+    });
   }
 
   /**
    * Sign a transaction and pass txHashHex+witness to the callback
    */
-  signTransaction(inputs, outputs, callback) {
+  signTransaction(
+    inputs: Array<InputTypeUTxO>,
+    outputs: Array<OutputTypeAddress | OutputTypeChange>,
+    callback: CallbackType
+  ): Promise<void> {
     return new Promise((resolve, reject) => {
         this._sendMessage({
           action: 'ledger-sign-transaction',
@@ -165,86 +224,82 @@ class LedgerBridge extends EventEmitter {
             }
           }
         })
-    })
+    });
   }
 
   // ================
   //   Bridge Setup
   // ================
 
-  _setupIframe () {
-    this.iframe = document.createElement('iframe')
-    this.iframe.src = this.bridgeUrl
-    document.head.appendChild(this.iframe)
-  }
-  _getOrigin () {
-    const tmp = this.bridgeUrl.split('/')
-    tmp.splice(-1, 1)
-    return tmp.join('/')
+  _getOrigin (): string {
+    const tmp = this.bridgeUrl.split('/');
+    tmp.splice(-1, 1);
+    return tmp.join('/');
   }
 
-  _sendMessage (msg, cb) {
-    msg.target = 'LEDGER-IFRAME'
-    this.iframe.contentWindow.postMessage(msg, '*')
+  _sendMessage (msg: MessageType, cb: Function) {
+    msg.target = 'LEDGER-IFRAME';
+    this.iframe.contentWindow.postMessage(msg, '*');
     window.addEventListener('message', ({ origin, data }) => {
-      if (origin !== this._getOrigin()) return false
+      if (origin !== this._getOrigin()) return false;
       if (data && data.action && data.action === `${msg.action}-reply`) {
-        cb(data)
+        cb(data);
       }
     })
   }
+}
 
-  // ====================
-  //   Helper Functions
-  // ====================
+// ====================
+//   Helper Functions
+// ====================
 
-  _getHdPath () {
-      return this._getPathForIndex(this.unlockedAccount);
+function _setupIframe (bridgeUrl: string): HTMLIFrameElement {
+  const iframe = document.createElement('iframe');
+  iframe.src = bridgeUrl;
+  
+  if (document.head) {
+    document.head.appendChild(iframe);
   }
 
-  __getPage (increment) {
+  return iframe;
+}
 
-    this.page += increment
+function _getHdPath(account: number): string {
+    return _getPathForIndex(account);
+}
 
-    if (this.page <= 0) { this.page = 1 }
-    const from = (this.page - 1) * this.perPage
-    const to = from + this.perPage
+function _getPage (page: number, perPage: number): Promise<Array<Account>> {
+  const from = (page - 1) * perPage;
+  const to = from + perPage;
 
-    return new Promise((resolve, reject) => {
-      this.unlock()
-        .then(async _ => {
-          let accounts = await this._getAccountsBIP44(from, to)
-          resolve(accounts)
-        })
-    })
-  }
+  return _getAccountsBIP44(from, to);
+}
 
-  async _getAccountsBIP44 (from, to) {
-    const accounts = []
+async function  _getAccountsBIP44 (from: number, to: number): Promise<Array<Account>> {
+  const accounts = [];
 
-    for (let i = from; i < to; i++) {
-      const path = this._getPathForIndex(i)
-      const address = await this.unlock(path)
-      const valid = await this._hasPreviousTransactions(address)
-      accounts.push({
-        address: address,
-        balance: null,
-        index: i,
-      })
-      // PER BIP44
-      // "Software should prevent a creation of an account if
-      // a previous account does not have a transaction history
-      // (meaning none of its addresses have been used before)."
-      if (!valid) {
-        break
-      }
+  for (let i = from; i < to; i++) {
+    const path = _getPathForIndex(i);
+    const address = await this.unlock(path);
+    const valid = await this._hasPreviousTransactions(address);
+    accounts.push({
+      address: address,
+      balance: null,
+      index: i,
+    });
+    // PER BIP44
+    // "Software should prevent a creation of an account if
+    // a previous account does not have a transaction history
+    // (meaning none of its addresses have been used before)."
+    if (!valid) {
+      break;
     }
-    return accounts
   }
+  return accounts;
+}
 
-  _getPathForIndex (index) {
-    return `m/44'/${coinType}'/${index}'/0/0`
-  }
+function _getPathForIndex (index: number): string {
+  return `m/44'/${coinType}'/${index}'/0/0`;
 }
 
 module.exports = LedgerBridge
